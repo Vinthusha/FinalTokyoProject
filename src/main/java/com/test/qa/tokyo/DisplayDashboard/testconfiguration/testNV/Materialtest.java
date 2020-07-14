8fb4b07edd8bfec482f890389f41dec6f01295af
Sathurya <sathurya14@gmail.com>
2020-07-14 13:51:38 +0530

    method base

---
+++ src/main/java/com/test/qa/tokyo/DisplayDashboard/testconfiguration/testNV/Materialtest.java
@@ -0,0 +1,4 @@
+package com.test.qa.tokyo.DisplayDashboard.testconfiguration.testNV;
+
+public class Materialtest {
+}
--- src/main/java/utils/MethodBase.java
+++ src/main/java/utils/MethodBase.java
@@ -1,9 +1,13 @@
 package utils;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.asserts.SoftAssert;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
@@ -394,5 +398,81 @@ public class MethodBase extends Constants {
         {
             System.out.println(window);
         }
+
+    }
+    public static boolean isDisplayed_ById(String locator) {
+        return  getDriver().findElement(By.id(locator)).isDisplayed();
+    }
+    public static boolean isDisplayed_ByXpath(String locator) {
+        return  getDriver().findElement(By.xpath(locator)).isDisplayed();
+    }
+    public static void setText_ByID(String locator,String inputText) {
+        getDriver().findElement(By.id(locator)).sendKeys(inputText);
+
+    }
+    public static void setText_ByXpath(String locator,String inputText) {
+        getDriver().findElement(By.xpath(locator)).sendKeys(inputText);
+
+    }
+    public static void setEnterKey_ByXpath(String locator) {
+        getDriver().findElement(By.xpath(locator)).sendKeys(Keys.ENTER);
+
+    }
+    public static void clickButton_ById(String locator) {
+        getDriver().findElement(By.id(locator)).click();
+    }
+    public static void clickButton_ByXpath(String locator) {
+        getDriver().findElement(By.xpath(locator)).click();
+    }
+    public static void click_ById(String locator) {
+
+        getDriver().findElement(By.id(locator)).click();
+    }
+    public static void click_ByName(String locator) {
+
+        getDriver().findElement(By.name(locator)).click();
+    }
+    public static void click_ByXpath(String locator) {
+        getDriver().findElement(By.xpath(locator)).click();
+
+    }
+
+    public static void clear_ById(String locator) {
+
+        getDriver().findElement(By.id(locator)).clear();
+    }
+    public static void clear_ByXpath(String locator) {
+
+        getDriver().findElement(By.xpath(locator)).clear();
+
+    }
+
+    public static void selectDropDown(String locator) {
+        Select dropdown = new Select(getDriver().findElement(By.xpath(locator)));
+        dropdown.selectByVisibleText("Database Testing");
+    }
+    public static void selectAction(String locator1 ,String locator2) {
+        Actions type = new Actions(getDriver());
+        type.moveToElement(getDriver().findElement(By.xpath(locator1))).click().build().perform();
+        type.click(getDriver().findElement(By.xpath(locator2))).build().perform();
+    }
+
+    public static String get_Text(String locator) {
+
+        return getDriver().findElement(By.xpath(locator)).getText();
+
+    }
+
+    public static void assertEqual_Text_ById(String locator, String expectedText) {
+
+        String actualText = getDriver().findElement(By.id(locator)).getText();
+
+        Assert.assertEquals(actualText, expectedText);
+    }
+    public static void assertEqual_Text_ByXpath(String locator, String expectedText) {
+        SoftAssert softAssert = new SoftAssert();
+        String actualText = getDriver().findElement(By.xpath(locator)).getText();
+        softAssert.assertEquals(actualText,expectedText);
+        softAssert.assertAll();
     }
 }
