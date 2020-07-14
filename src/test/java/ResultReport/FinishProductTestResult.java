82e36c4240f683426f470c9ccec5fa385ee35321
Daclas95 <daclas95@gmail.com>
2020-07-14 19:26:48 +1000

    Dac

---
+++ src/test/java/ResultReport/FinishProductTestResult.java
@@ -0,0 +1,14 @@
+package ResultReport;
+
+import com.test.qa.utils.TestBase;
+import org.testng.annotations.Test;
+import utils.MethodBase;
+
+public class FinishProductTestResult extends TestBase {
+
+    @Test
+    public void FinishProductTestResult(){
+        MethodBase.click_ByXpath("//*[@id=\"root\"]/div/section/section/main/div/div[1]/div[1]/a[3]/div/div");
+        MethodBase.click_ByXpath("//*[@id=\"root\"]/div/section/section/main/div/div[1]/div[2]\n");
+    }
+}
