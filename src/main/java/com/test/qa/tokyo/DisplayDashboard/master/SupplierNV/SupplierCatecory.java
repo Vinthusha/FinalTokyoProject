f4244cb635a00cdc9c715b92887a45c8b5b930c1
vinthusha <vinthushalogan@gmail.com>
2020-07-15 11:07:20 +0530

    employee data

--- src/main/java/com/test/qa/tokyo/DisplayDashboard/master/SupplierNV/SupplierCatecory.java
+++ src/main/java/com/test/qa/tokyo/DisplayDashboard/master/SupplierNV/SupplierCatecory.java
@@ -13,17 +13,16 @@ public class SupplierCatecory extends PageBase {
 
     private static final Logger LOGGER = (Logger) Logger.getLogger(String.valueOf(PlantPanel.class));
 
+    private static By masterMenu = By.xpath("//*[@id=\"root\"]/div/section/section/main/div/div[1]/div[1]/a[1]/div/div");
     private static By MenuTabSupplier = By.xpath("//*[@id=\"root\"]/div/section/section/header/ul/li[13]/a");
-
-    private static By plantSuplierCatPanel = By.xpath("//*[@id=\"root\"]/div/section/section/header/ul/li[14]/a");
     private static By SupplierCategory = By.xpath("//*[@id=\"root\"]/div/section/section/main/div/div[1]/div[1]");
     private static By AddCatecory = By.xpath("//*[@id=\"root\"]/div/section/section/main/div/div[2]/div/div/div/div[1]/div/div[2]/button");
     private static By txtCatecory = By.id("supplier_category");
     private static By txtsupplierCategory_description = By.id("supplierCategory_description");
 
-    private static By butPlantSave = By.xpath("/html/body/div[3]/div/div[2]/div/div[2]/div[3]/button[2]");
+    private static By butSave = By.xpath("/html/body/div[3]/div/div[2]/div/div[2]/div[3]/button[2]");
 
-    private static By masterMenu = By.xpath("//*[@id=\"root\"]/div/section/section/main/div/div[1]/div[1]/a[1]/div/div");
+
 
 
 
@@ -32,10 +31,6 @@ public class SupplierCatecory extends PageBase {
         getDriver().findElement(MenuTabSupplier).click();
     }
 
-    public static void clickplantSuplierCatPanel() {
-
-        getDriver().findElement(plantSuplierCatPanel).click();
-    }
     public static void clickSupplierCategory() {
 
         getDriver().findElement(SupplierCategory).click();
@@ -54,16 +49,18 @@ public class SupplierCatecory extends PageBase {
 
         getDriver().findElement(txtsupplierCategory_description).sendKeys(supplierCategory_description);
     }
-    public static void clickplantSave() {
+    public static void clickSave() {
 
-        getDriver().findElement(butPlantSave).click();
+        getDriver().findElement(butSave).click();
 
     }
     public static void clickMasterPanel() {
 
         getDriver().findElement(masterMenu).click();
-
-
     }
 
+    public static boolean isDashboardDisplay() {
+        return  getDriver().findElement(masterMenu).isDisplayed();
+
+    }
 }
--- src/test/java/master/SupplierCatecory/SupplierCatecoryTest.java
+++ src/test/java/master/SupplierCatecory/SupplierCatecoryTest.java
@@ -1,20 +1,24 @@
 package master.SupplierCatecory;
 
+import com.test.qa.testdata.masterdata.SupplierCatecoryData;
 import com.test.qa.tokyo.DisplayDashboard.master.SupplierNV.SupplierCatecory;
 import com.test.qa.utils.TestBase;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 public class SupplierCatecoryTest extends TestBase {
 
-    @Test(groups = {"Smoke"}, priority = 1)
-    public void SupplierCatecorySmokeTest() {
+    @Test(groups = {"Smoke"}, priority = 1, dataProviderClass = SupplierCatecoryData.class,dataProvider = "Supplierdataprovider")
+    public void SupplierCatecorySmokeTest(String Category, String Description) {
+        softAssert = new SoftAssert();
+        softAssert.assertTrue(SupplierCatecory.isDashboardDisplay(),"is not displayed ");
         SupplierCatecory.clickMasterPanel();
         SupplierCatecory.clickMenuTabSupplier();
         SupplierCatecory.clickSupplierCategory();
         SupplierCatecory.clickAddCatecory();
-        SupplierCatecory.setsupplier_category("sandbulk");
-        SupplierCatecory.setsupplierCategory_description("collect");
-        SupplierCatecory.clickplantSave();
+        SupplierCatecory.setsupplier_category(Category);
+        SupplierCatecory.setsupplierCategory_description(Description);
+        SupplierCatecory.clickSave();
 
     }
 }
